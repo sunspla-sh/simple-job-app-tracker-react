9448f8b34e87759a3e002be9dc2a761f42a811f6
jack <jrfisch95@gmail.com>
2022-10-16 07:29:53 -0400

    finish navbar component for desktop and add some desktop navbar styling

--- apps/web/src/components/Navbar.jsx
+++ apps/web/src/components/Navbar.jsx
@@ -46,6 +46,7 @@ export const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='navbar_header'>
+        {/* MOBILE HEADER WITH LOGO AND DROPDOWN BUTTON */}
         <div className='navbar_header-mobile'>
           <div>
             <Link
@@ -83,10 +84,59 @@ export const Navbar = () => {
               </svg>
           </div>
         </div>
+        {/* DESKTOP HEADER WITH LOGO AND NAVIGATION LINKS */}
         <div className='navbar_header-desktop'>
-          {/* links to all pages in a row */}
+          <div className='navbar_header-desktop_logo-container'>
+            <Link
+              to='/'
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className='svg_logo'
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+              </svg>
+            </Link>
+          </div>
+          <div className='navbar_header-desktop_link-container'>
+            {isLoggedIn ? (
+            <>
+              {loggedInNavigation.map(element => (
+                <NavLink
+                  key={element.to}
+                  to={element.to}
+                  className={({ isActive }) => isActive ? 'navbar_header-desktop_link active' : 'navbar_header-desktop_link'}
+                >
+                  {element.name}
+                </NavLink>
+              ))}
+              <button
+                className='navbar_header-desktop_logout'
+                onClick={logOutUser}
+              >
+                Log Out
+              </button>
+            </>
+          ) : (
+            loggedOutNavigation.map(element => (
+              <NavLink
+                key={element.to}
+                to={element.to}
+                className={({ isActive }) => isActive ? 'navbar_header-desktop_link active' : 'navbar_header-desktop_link'}
+              >
+                {element.name}
+              </NavLink>
+            )).reverse()
+          )}
+          </div>
         </div>
+          
       </div>
+      {/* MOBILE DROPDOWN */}
       <div
         className={isOpen ? 'navbar_dropdown' : 'hidden'}
         onClick={() => setIsOpen(false)}

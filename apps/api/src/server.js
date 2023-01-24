3431b6b25f78b82e650026b52f0ffc8c97df2584
jack <jrfisch95@gmail.com>
2023-01-24 00:03:31 -0500

    add daily report functionality with cron job at midnight each day. add favicon to webapp. still need to get out of aws email sandbox, deploy to fly.io, and separate cron task from server.js to report.task.js

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,5 +1,10 @@
 import './config.js'; //should be first because dotenv happens in here
 
+import { Temporal, Intl, toTemporalInstant } from '@js-temporal/polyfill';
+Date.prototype.toTemporalInstant = toTemporalInstant;
+import { STATUS_ENUM } from './utils/statusEnum.js';
+import { sendEmail } from './utils/email/sendEmail.js';
+
 import { createServer as createHttpServer } from 'http';
 
 import cron from 'node-cron';
@@ -21,10 +26,53 @@ const server = createHttpServer(app);
 ws.attach(server);
 
 //attach task that reports user jobs at midnight and emits a jobapp:daily-count-reset event
-const task = cron.schedule('0 0 0 * * *', () => {
+const task = cron.schedule('0 0 0 * * *', async () => {
+// const task = cron.schedule('*/10 * * * * *', async () => {
   ws.emit('jobapp:daily-count-reset', 0);
 
-  //get all users for the day and each of their job apps
+  try {
+    //get current instant
+    const now = Temporal.Now.instant();
+
+    //get yesterday's first millisecond in US Eastern timezone (take current time, subtract one day, round down to start of day)
+    const yesterdayFirstMillisecond = Temporal.Instant.from(now).toZonedDateTimeISO('America/New_York').subtract({ days: 1 }).round({ smallestUnit: 'day', roundingMode: 'floor'}).epochMilliseconds;
+    //get yesterday's last millisecond in US Eastern timezone (take current time, round down to start of day, substract one millisecond)
+    const yesterdayLastMillisecond = Temporal.Instant.from(now).toZonedDateTimeISO('America/New_York').round({ smallestUnit: 'day', roundingMode: 'floor'}).subtract({ milliseconds: 1 }).epochMilliseconds;
+    //get all users for the day and each of their job apps
+    const users = await prisma.user.findMany({
+      include: {
+        jobApps: true
+      }
+    });
+    const usersWithStats = users.map(u => {
+      return {
+        ...u,
+        totalJobApps: u.jobApps.length,
+        stats: u.jobApps.reduce((acc, cur) => {
+          const createdAt = new Date(cur.createdAt).toTemporalInstant().epochMilliseconds;
+          acc.dailyCount = acc.dailyCount + ((yesterdayFirstMillisecond <= createdAt && yesterdayLastMillisecond >= createdAt) ? 1 : 0);
+          acc.applied = acc.applied + ((cur.status === STATUS_ENUM[0]) ? 1 : 0);
+          acc.interviewing = acc.interviewing + ((cur.status === STATUS_ENUM[1]) ? 1 : 0);
+          acc.noOffer = acc.noOffer + ((cur.status === STATUS_ENUM[2]) ? 1 : 0);
+          acc.offerReceived = acc.offerReceived + ((cur.status === STATUS_ENUM[2]) ? 1 : 0);
+          return acc;
+        }, { dailyCount: 0, applied: 0, interviewing: 0, noOffer: 0, offerReceived: 0 })
+      }
+    });
+
+    const result = await sendEmail({
+      emailTo: process.env.FROM_EMAIL,
+      emailSubject: 'Daily Job App Report',
+      handlebarsVariables: { usersArray: usersWithStats },
+      handlebarsTemplate: 'daily-report.hbs'
+    });
+
+    console.log('email for daily job app report sent successfully', result);
+    
+  } catch (err) {
+    console.log(err);
+  }
+  
 }, {
   scheduled: false,
   timezone: 'America/New_York'

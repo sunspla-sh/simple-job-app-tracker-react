7ed8eb982b070be6900e295e21479e5c1cd7595f
jack <jrfisch95@gmail.com>
2023-01-30 22:31:59 -0500

    add job offer websites to faq section - ycombinator, builtin, techstars, wellfound (angellist talent), and remoteok

--- apps/web/src/pages/FAQPage.jsx
+++ apps/web/src/pages/FAQPage.jsx
@@ -11,18 +11,35 @@ export const FAQPage = () => {
         >
           FAQs
         </h1>
-        <p
+        <div
           className="faq_note"
         >
           {/* 
-            ycombinator
-            jobs.techstars.com/jobs
-            angellist
-            builtin
             remoteok
           */}
-          FAQs are coming soon...
-        </p>
+          <ol className="faq_list">
+            <li className="faq_list-item">
+              <p>Where can I find software jobs?</p>
+              <ul>
+                <li>
+                  <a href="https://www.ycombinator.com/jobs/role/software-engineer" target="_blank">YCombinator</a>
+                </li>
+                <li>
+                  <a href="https://jobs.techstars.com/jobs?filter=eyJqb2JfZnVuY3Rpb25zIjpbIlNvZnR3YXJlIEVuZ2luZWVyaW5nIl19" target="_blank">TechStars</a>
+                </li>
+                <li>
+                  <a href="https://angel.co/role/software-engineer" target="_blank">Wellfound (formerly AngelList Talent)</a>
+                </li>
+                <li>
+                  <a href="https://builtin.com/jobs/dev-engineering" target="_blank">BuiltIn</a>
+                </li>
+                <li>
+                  <a href="https://remoteok.com/remote-engineer-jobs" target="_blank">RemoteOK</a>
+                </li>
+              </ul>
+            </li>
+          </ol>
+        </div>
       </div>
       
     </div>
